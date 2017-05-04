37e0d2e61ff050a69dd64396c4253a4f9a67e5eb
Arthur Mendes Lopes <arthur_dast@hotmail.com>
2017-05-04 16:16:18 -0300

    Adicionando imagem
    colocando um exemplo de imagem

--- Lp1_projeto_final/src/br/com/UFPB/Lp1_projeto_final/entidades/Estoque.java
+++ Lp1_projeto_final/src/br/com/UFPB/Lp1_projeto_final/entidades/Estoque.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.UFPB.Lp1_projeto_final.entidades;
 
-/**
- *
- * @author arthur
- */
+
 public class Estoque {
+
+    private int cod_produto;
+    private String nome;
+    private int estoque;
+    double precoCusto;
+    double precoVenda;
     
+            
 }
