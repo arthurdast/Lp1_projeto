cc807a7f82e938717df41d9e234c496c9abe3ccb
Arthur Mendes Lopes <arthur_dast@hotmail.com>
2017-05-04 16:23:05 -0300

    ESTOQUE

--- Lp1_projeto_final/src/br/com/UFPB/Lp1_projeto_final/entidades/Estoque.java
+++ Lp1_projeto_final/src/br/com/UFPB/Lp1_projeto_final/entidades/Estoque.java
@@ -1,14 +1,66 @@
 
 package br.com.UFPB.Lp1_projeto_final.entidades;
 
+import java.awt.image.BufferedImage;
+
 
 public class Estoque {
 
     private int cod_produto;
     private String nome;
     private int estoque;
-    double precoCusto;
-    double precoVenda;
+    private double precoCusto;
+    private double precoVenda;
+    private BufferedImage imagem;
+
+    public int getCod_produto() {
+        return cod_produto;
+    }
+
+    public void setCod_produto(int cod_produto) {
+        this.cod_produto = cod_produto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public BufferedImage getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(BufferedImage imagem) {
+        this.imagem = imagem;
+    }
+       
     
-            
+    
 }
