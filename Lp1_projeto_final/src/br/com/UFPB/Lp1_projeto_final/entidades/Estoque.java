e49a4238fe684a1c35ab79fa2d847dfa935e46b6
Arthur Mendes Lopes <arthur_dast@hotmail.com>
2017-05-06 15:45:41 -0300

    Produto
    criação da classe produto

--- Lp1_projeto_final/src/br/com/UFPB/Lp1_projeto_final/entidades/Estoque.java
+++ Lp1_projeto_final/src/br/com/UFPB/Lp1_projeto_final/entidades/Estoque.java
@@ -1,66 +1,22 @@
 
 package br.com.UFPB.Lp1_projeto_final.entidades;
 
-import java.awt.image.BufferedImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class Estoque {
+    
+    List<Produto> nomeDoArrayList ;
 
-    private int cod_produto;
-    private String nome;
-    private int estoque;
-    private double precoCusto;
-    private double precoVenda;
-    private BufferedImage imagem;
-
-    public int getCod_produto() {
-        return cod_produto;
+    public Estoque() {
+        nomeDoArrayList = new ArrayList<>();
     }
-
-    public void setCod_produto(int cod_produto) {
-        this.cod_produto = cod_produto;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getEstoque() {
-        return estoque;
-    }
-
-    public void setEstoque(int estoque) {
-        this.estoque = estoque;
-    }
-
-    public double getPrecoCusto() {
-        return precoCusto;
-    }
-
-    public void setPrecoCusto(double precoCusto) {
-        this.precoCusto = precoCusto;
-    }
-
-    public double getPrecoVenda() {
-        return precoVenda;
-    }
-
-    public void setPrecoVenda(double precoVenda) {
-        this.precoVenda = precoVenda;
-    }
-
-    public BufferedImage getImagem() {
-        return imagem;
-    }
-
-    public void setImagem(BufferedImage imagem) {
-        this.imagem = imagem;
-    }
-       
+    
+    
+    
     
     
 }
--- Lp1_projeto_final/src/br/com/UFPB/Lp1_projeto_final/entidades/Produto.java
+++ Lp1_projeto_final/src/br/com/UFPB/Lp1_projeto_final/entidades/Produto.java
@@ -5,10 +5,84 @@
  */
 package br.com.UFPB.Lp1_projeto_final.entidades;
 
+import java.awt.image.BufferedImage;
+
 /**
  *
  * @author arthur
  */
 public class Produto {
     
+     private int cod_produto;
+    private String nome;
+    private String secao;
+    private String unidade;
+    private int estoque;
+    private double precoCusto;
+    private double precoVenda;
+    private BufferedImage imagem;
+
+    public String getSecao() {
+        return secao;
+    }
+
+    public void setSecao(String secao) {
+        this.secao = secao;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public int getCod_produto() {
+        return cod_produto;
+    }
+
+    public void setCod_produto(int cod_produto) {
+        this.cod_produto = cod_produto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public double getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public BufferedImage getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(BufferedImage imagem) {
+        this.imagem = imagem;
+    }
 }
